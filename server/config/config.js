457f4f1a459b2574b5a355a8e9abcf7fa3198ea4
FranciscoUribeSM <panchodafm@gmail.com>
2018-09-06 18:42:41 -0400

    REST SERVER USUARIO

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,22 @@
 
 process.env.PORT =  process.env.PORT || 3000; 
 
+/**
+ * ENTORNO
+ */
+
+process.env.NODE_ENV =  process.env.PORNODE_ENVT || 'dev';
+
+/**
+ * Base dedatos
+ */
+
+ let urlDB; 
+
+//  if(process.env.NODE_ENV  =='dev'){
+//      urlDB='mongodb://localhost:27017/cafe';
+//  }else{
+    urlDB='mongodb://cafe-user:1234asdASD@ds247852.mlab.com:47852/cafe';
+//  }
+
+ process.env.URLDB = urlDB; 
